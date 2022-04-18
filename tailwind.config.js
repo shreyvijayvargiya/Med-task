4c2bf6d53b300bd586e03e8849ea48a6f22a0a4b
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2022-04-18 19:41:03 +0530

    purge edited in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    "./pages/**/.{js,ts,jsx,tsx}",
-    "./components/**/.{js,ts,jsx,tsx}",
-    "./modules/**/.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./modules/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class',
   theme: {
